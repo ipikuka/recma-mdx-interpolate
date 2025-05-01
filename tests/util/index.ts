8aa3fa7d6000a37dd95cae16caa1c0d71264471d
talatkuyuk <talatkuyuk@gmail.com>
2025-05-01 10:04:59 +0300

    Update the test util

--- tests/util/index.ts
+++ tests/util/index.ts
@@ -1,4 +1,5 @@
 import { compile as compileOriginal, type CompileOptions } from "@mdx-js/mdx";
+import rehypeRaw from "rehype-raw";
 
 import recmaMdxInterpolate, { type InterpolateOptions } from "../../src";
 
@@ -6,11 +7,13 @@ export const compile = async (
   source: string,
   options?: CompileOptions & InterpolateOptions,
 ) => {
-  const { exclude = {}, disable = false, ...rest } = options ?? {};
+  const { exclude = {}, disable = false, format = "mdx", ...rest } = options ?? {};
 
   return String(
     await compileOriginal(source, {
+      format,
       ...rest,
+      rehypePlugins: format === "md" ? [rehypeRaw] : undefined,
       recmaPlugins: [[recmaMdxInterpolate, { exclude, disable }]],
     }),
   );
@@ -20,12 +23,14 @@ export const compileJsx = async (
   source: string,
   options?: CompileOptions & InterpolateOptions,
 ) => {
-  const { exclude = {}, disable = false, ...rest } = options ?? {};
+  const { exclude = {}, disable = false, format = "mdx", ...rest } = options ?? {};
 
   return String(
     await compileOriginal(source, {
+      format,
       ...rest,
       jsx: true,
+      rehypePlugins: format === "md" ? [rehypeRaw] : undefined,
       recmaPlugins: [[recmaMdxInterpolate, { exclude, disable }]],
     }),
   );
